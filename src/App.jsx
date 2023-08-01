c2ecccd4fe6dff290e7a9d2ba04a47d7f8c4e896
¨mago¨ <martingramajo08@gmail.com>
2023-08-01 12:59:42 -0300

    agrego la consulta a la API y la guardo en el state dataClima

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,28 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import CardClima from "./components/CardClima";
 import Formulario from "./components/Formulario";
 
 function App() {
+  const [dataClima, setDataClima] = useState({});
+  console.log("file: App.jsx:8 ~ App ~ dataClima:", dataClima);
+
+  useEffect(() => {
+    consultarApi();
+  }, []);
+
+  const consultarApi = async () => {
+    try {
+      const API_URL = `https://api.openweathermap.org/data/2.5/weather?q=tucuman,ar&appid=ce25853294f1642f1da64c29b939a302&units=metric&lang=es`;
+      const respuesta = await fetch(API_URL);
+
+      const dato = await respuesta.json();
+      setDataClima(dato);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="bg-app d-flex flex-column min-vh-100">
       <Formulario />
