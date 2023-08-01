59e1fdb2a13e054fbea8cd909dbba60a5da08e5d
¨mago¨ <martingramajo08@gmail.com>
2023-08-01 19:55:47 -0300

    Utilizo el react hook form para crear mi formulario

--- src/App.jsx
+++ src/App.jsx
@@ -2,30 +2,73 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import CardClima from "./components/CardClima";
 import Formulario from "./components/Formulario";
+import { useForm } from "react-hook-form";
+import Swal from "sweetalert2";
 
 function App() {
   const [dataClima, setDataClima] = useState({});
-  console.log("file: App.jsx:8 ~ App ~ dataClima:", dataClima);
+  const [dataForm, setDataForm] = useState({});
+  console.log("file: App.jsx:11 ~ App ~ dataForm:", dataForm);
 
-  useEffect(() => {
-    consultarApi();
-  }, []);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm();
 
-  const consultarApi = async () => {
-    try {
-      const API_URL = `https://api.openweathermap.org/data/2.5/weather?q=tucuman,ar&appid=ce25853294f1642f1da64c29b939a302&units=metric&lang=es`;
-      const respuesta = await fetch(API_URL);
-
-      const dato = await respuesta.json();
-      setDataClima(dato);
-    } catch (error) {
-      console.log(error);
+  const onSubmit = (data) => {
+    if (
+      errors.city?.type !== "required" &&
+      errors.country?.type !== "required"
+    ) {
+      setDataForm(data);
+      Swal.fire({
+        icon: "success",
+        title: "Datos Enviado. Espere su consulta por favor.",
+        showConfirmButton: false,
+        timer: 2000,
+      });
+      reset();
+    } else {
+      Swal.fire({
+        icon: "error",
+        title: "No se pudo enviar los datos.",
+      });
     }
   };
+  // usamos el hook useForm de la librería. El cual devuelve un objeto.
+  // register: function que permite registrar los distintos campos del form.
+  // handleSubmit: para gestionar la acción del submit del formulario.
+  // usamos el handleSubmit a la cual le pasamos la función onSubmit
+  // usamos el register en los form.control o input, para cargar los input al objeto del form
+  // en register podemos agregar un segundo parametro {}, para hacerlo requerido al input o agregarle la longitud max.
+  // con la propiedad {errors} la podemos utilizar para mostrar un msj de error al usuario, para sacar esta propiedad utilizamos formState
+
+  // useEffect(() => {
+  //   consultarApi();
+  // }, []);
+
+  // const consultarApi = async () => {
+  //   try {
+  //     const API_URL = `https://api.openweathermap.org/data/2.5/weather?q=tucuman,ar&appid=ce25853294f1642f1da64c29b939a302&units=metric&lang=es`;
+  //     const respuesta = await fetch(API_URL);
+
+  //     const dato = await respuesta.json();
+  //     setDataClima(dato);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
 
   return (
     <div className="bg-app d-flex flex-column min-vh-100">
-      <Formulario />
+      <Formulario
+        register={register}
+        handleSubmit={handleSubmit}
+        onSubmit={onSubmit}
+        errors={errors}
+      />
       <CardClima />
     </div>
   );
--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,44 +1,41 @@
-import { useState } from "react";
 import { Button } from "react-bootstrap";
 import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
 
-const Formulario = () => {
-  const [validated, setValidated] = useState(false);
-
-  const handleSubmit = (event) => {
-    const form = event.currentTarget;
-    if (form.checkValidity() === false) {
-      event.preventDefault();
-      event.stopPropagation();
-    }
-
-    setValidated(true);
-  };
-
+const Formulario = ({ register, handleSubmit, onSubmit, errors }) => {
   return (
     <div className="d-flex justify-content-center py-5">
       <Form
-        onSubmit={handleSubmit}
+        onSubmit={handleSubmit(onSubmit)}
         className="p-4 form-box"
-        noValidate
-        validated={validated}
         style={{ borderRadius: "10px" }}
       >
         <Row className="mb-3 ">
           <Form.Group className="user-box" controlId="validationCustom01">
-            <Form.Control className="ps-2" name="city" required type="text" />
+            <Form.Control
+              className="ps-2"
+              {...register("city", { required: true })}
+              type="text"
+            />
             <Form.Label className="ms-3 h6">Nombre de la Ciudad</Form.Label>
-            <Form.Control.Feedback className="mb-2" type="invalid">
-              Por favor ingresar una ciudad.
-            </Form.Control.Feedback>
+            {errors.city?.type === "required" && (
+              <p className="text-danger peso-bold">
+                Por favor ingresa un nombre de ciudad valida.
+              </p>
+            )}
           </Form.Group>
+
           <Form.Group className="my-2 user-box" controlId="validationCustom02">
-            <Form.Control name="country" required type="text" />
+            <Form.Control
+              type="text"
+              {...register("country", { required: true })}
+            />
             <Form.Label className=" ms-3  h6">Nombre del país</Form.Label>
-            <Form.Control.Feedback type="invalid">
-              Por favor ingresa un país
-            </Form.Control.Feedback>
+            {errors.country?.type === "required" && (
+              <p className="text-danger peso-bold">
+                Por favor ingresa un nombre de País valido.
+              </p>
+            )}
           </Form.Group>
         </Row>
         <div className="d-flex justify-content-center">
