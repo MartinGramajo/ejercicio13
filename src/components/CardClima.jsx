6f5e7239ab22d7770c62d9d3d910999a7043fa78
¨mago¨ <martingramajo08@gmail.com>
2023-08-01 20:34:54 -0300

    agrego una nueva card para mostrar la data del clima solicitada

--- src/components/CardClima.jsx
+++ src/components/CardClima.jsx
@@ -1,18 +1,44 @@
-import { Card } from "react-bootstrap";
+import { Slide } from "@mui/material";
 
-const CardClima = () => {
+const CardClima = ({ datos }) => {
   return (
-    <div className="d-flex justify-content-center">
-      <Card style={{ width: "18rem" }}>
-        <Card.Header>Nombre de la ciudad</Card.Header>
-        <Card.Body>
-          <Card.Title>temperatura: </Card.Title>
-          <Card.Text>temperatura:</Card.Text>
-          <Card.Text>humedad:</Card.Text>
-          <Card.Text>viento:</Card.Text>
-          <Card.Text>sensación térmica:</Card.Text>
-        </Card.Body>
-      </Card>
+    <div className="container">
+      <div className="py-5">
+        <h1 className="city text-center">
+          {datos.name}, {datos.sys.country}
+        </h1>
+        <div className="group d-flex justify-content-center">
+          <img
+            src={`http://openweathermap.org/img/wn/${datos.weather[0].icon}.png`}
+            alt=""
+          />
+          <h1 className=" temp ">{datos.weather[0].description}</h1>
+        </div>
+        <div className="text-center"></div>
+        <Slide direction="right" timeout={800} in={true}>
+          <div className="box_container">
+            <div className="box">
+              <p>Temperatura</p>
+              <h1 className="">{datos.main.temp} °C</h1>
+            </div>
+
+            <div className="box">
+              <p>Humedad</p>
+              <h1>{datos.main.humidity}%</h1>
+            </div>
+
+            <div className="box">
+              <p>Viento</p>
+              <h1>{datos.wind.speed}km/h</h1>
+            </div>
+
+            <div className="box">
+              <p>Sensación térm</p>
+              <h1>{datos.main.feels_like}°C</h1>
+            </div>
+          </div>
+        </Slide>
+      </div>
     </div>
   );
 };
